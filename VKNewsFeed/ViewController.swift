0abaa09f36a406858f441787e9e742ad9a96cd6d
Tony Po <antoxapa@gmail.com>
2020-02-14 18:50:28 +0300

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Антон Потапчик on 2/14/20.
+//  Copyright © 2020 TonyPo Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
